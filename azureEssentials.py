716fa5458657aa33602b5ef800d26cd5324eeaab
Strauss Botond V <d21_strauss.botond@vargaszolnok.hu>
2021-09-10 11:22:08 +0200

    Mappák, rendezés

--- azureEssentials.py
+++ azureEssentials.py
@@ -1,9 +1,12 @@
 ﻿import os
 dir = str(os.path.dirname(os.path.abspath(__file__)))
+
 def localize(file):
     return(dir + "\\" + str(file))
+
 def playSound(index):
-    os.system(str(dir) + "\sound.bat " + str(index))
+    os.system(str(dir) + "\Batch\sound.bat " + str(index))
+
 def isWeekDay(day):
     if day == "Mon" or day == "Tue" or day == "Wed" or day == "Thu" or day == "Fri":
         return(True)
--- output.py
+++ output.py
@@ -1,5 +1,7 @@
-﻿import rshell
-import time
+﻿import time
+import os
+
+dir = str(os.path.dirname(os.path.abspath(__file__)))
 
 def pir(onoff):
     if onoff == 1:
